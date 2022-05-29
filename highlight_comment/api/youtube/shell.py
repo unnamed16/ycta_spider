5cf8cc0050d5626887dd3eba7d1dcfdedeaef61b
kuyaki <kfiktivni@yandex.ru>
2022-05-29 23:10:30 +0300

    hotfix

--- highlight_comment/api/youtube/shell.py
+++ highlight_comment/api/youtube/shell.py
@@ -18,37 +18,36 @@ class Shell(CommonShell):
     def __init__(self):
         super().__init__()
         self.__platform_type = PlatformType.YOUTUBE
-        self.__host = 'https://www.googleapis.com/youtube/v3/'
+        self.__url = 'https://www.googleapis.com/youtube/v3/'
         platform_config = self.config['platforms'][self.platform_type.name]
         self.__api_key = platform_config['api_key']
         self.__access_token = platform_config['access_token']
-        self.__headers = dict(self.common_headers)
 
     def get_comments(self, source: SourceUri) -> CommentsResponse:
-        url = 'commentThreads'
+        func = 'commentThreads'
         part = 'part=snippet,replies'
-        q = f'{self.__host}{url}?{part}&{source}&key={self.__api_key}'
-        comments = requests.get(q, headers=self.__headers)
+        query = f'{self.__url}{func}?{part}&{source}&key={self.__api_key}'
+        comments = requests.get(query, headers=self.common_headers)
         return Shell.__parse(comments, Shell.__parse_comments)
 
     def add_comment(self, source: SourceUri, comment: str) -> ResponseCode:
-        url = 'commentThreads'
+        func = 'commentThreads'
         part = 'part=snippet'
-        q = f'{self.__host}{url}?{part}&{source}&key={self.__api_key}'
-        headers = dict(self.__headers)
+        query = f'{self.__url}{func}?{part}&{source}&key={self.__api_key}'
+        headers = dict(self.common_headers)
+        headers['Authorization'] = f'Bearer {self.__access_token}'
         data = {
             "snippet": {
                 "channelId": self.config['platforms'][self.platform_type.name]['channel_id'],
                 "topLevelComment": {
                     "snippet": {
-                        "textOriginal": f"{comment}"
+                        "textOriginal": comment
                     }
                 },
                 "videoId": "i2FGXF540pU"
             }
         }
-        headers['Authorization'] = f'Bearer {self.__access_token}'
-        comments = requests.post(q, headers=headers, data=data)
+        comments = requests.post(query, headers=headers, data=data)
         return Shell.__parse(comments, Shell.__parse_comments)
 
     @staticmethod
