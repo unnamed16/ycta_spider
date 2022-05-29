06a77297b7b0ec7a5ac56185f70701b0a62e99d3
kuyaki <kfiktivni@yandex.ru>
2022-05-30 01:46:47 +0300

    added test files for reader and writer (not implemented yet)

---
+++ test/file_manager/test_writer.py
@@ -0,0 +1,16 @@
+__licence__ = 'MIT'
+__author__ = 'kuyaki'
+__credits__ = ['kuyaki']
+__maintainer__ = 'kuyaki'
+__date__ = '2022/05/30'
+
+from unittest import TestCase
+
+
+class WriterTestCase(TestCase):
+
+    def test_save_file(self):
+        pass
+
+    def test_save_json(self):
+        pass
